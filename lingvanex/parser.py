515a64a587c475bb76fca596eb23bcffc3ea1c19
GoldenAnpu <sych.sergius@gmail.com>
2022-08-25 01:24:44 +0300

    OOP

--- lingvanex/parser.py
+++ lingvanex/parser.py
@@ -1,54 +1,57 @@
-def try_to_save(filename, answer=''):
-    """ Decides to create or open an existing file for the following interaction
-
-    :param filename: name for txt file
-    :param answer: mode for open()
-    :return: list [file, answer]
-    """
-    try:
-        file = open(f'{filename}.txt', 'x', encoding='UTF-8')
-        answer = 'X'
-        return file, answer
-    except FileExistsError:
-        print(f'\nFile {filename}.txt already exists')
-        while answer not in ['A', 'O']:
-            answer = input(' - type O if you want to overwrite\n'
-                           ' - type A if you want to append\n\n'
-                           'Your choice: ')
-        if 'O' in answer:
-            print(f'\n{filename}.txt overwrote')
-            file = open(f'{filename}.txt', 'w', encoding='UTF-8')
-            return file, answer
-        elif 'A' in answer:
-            print(f'\n{filename}.txt extended')
-            file = open(f'{filename}.txt', 'a', encoding='UTF-8')
-            return file, answer
-        elif 'X' in answer:
-            file = open(f'{filename}.txt', 'x', encoding='UTF-8')
-            return file, answer
+from datetime import datetime
+import calendar
 
 
-received_file = 'PythonTest.txt'
+class FileParser:
+    def __init__(self, filename):
+        self.filename = filename
+        self.native = 'en'
+        self.translate = 'ru'
 
-try:
-    with open(received_file, encoding='UTF-8') as test_file:
-        lines_list = test_file.readlines()
-except FileNotFoundError:
-    print(f'\nPlease drop {received_file} file in the same directory as parser.py and run again')
-    exit()
+    def read_file(self):
+        try:
+            with open(self.filename, encoding='UTF-8') as test_file:
+                lines_list = test_file.readlines()
+                return lines_list
+        except FileNotFoundError:
+            print(f'\nPlease drop {self.filename} file in the same directory as parser.py and run again')
+            exit()
 
-english_file = try_to_save('English')
-russian_file = try_to_save('Russian', english_file[1])  # operation must be the same for both files
+    def split_lines_by_language(self):
+        lines_list = self.read_file()
+        native_file = self.create_file_to_save(self.native)
+        translate_file = self.create_file_to_save(self.translate)
+        for line in lines_list:
+            if not line.startswith('#') and line != '\n':  # don't need to process unnecessary lines
+                line = line.replace('\n', '')  # to avoid empty lines
+                dictionary = line.split('\t')
+                native_list = list(dict.fromkeys(dictionary[0].split(' ; ')))  # remove duplicates after splitting
+                translate_list = list(dict.fromkeys(dictionary[1].split(' ; ')))  # remove duplicates after splitting
+                for native_item in native_list:
+                    for translate_item in translate_list:
+                        native_file.write(f'{native_item}\n')
+                        translate_file.write(f'{translate_item}\n')
+        print('Done')
 
-for line in lines_list:
-    if not line.startswith('#') and not line.startswith('\n'):  # don't need to process unnecessary lines
-        line = line.replace('\n', '')  # to avoid empty lines
-        dictionary = line.split('\t')
-        english_list = list(dict.fromkeys(dictionary[0].split(' ; ')))  # remove duplicates after splitting
-        russian_list = list(dict.fromkeys(dictionary[1].split(' ; ')))  # remove duplicates after splitting
-        for english_item in english_list:
-            for russian_item in russian_list:
-                english_file[0].write(f'{english_item}\n')
-                russian_file[0].write(f'{russian_item}\n')
-english_file[0].close()
-russian_file[0].close()
+    @staticmethod
+    def create_file_to_save(language):
+        if language == 'en':
+            filename_save = 'English'
+        elif language == 'ru':
+            filename_save = 'Russian'
+        else:
+            filename_save = 'indefinite'
+
+        try:
+            file = open(f'{filename_save}.txt', 'x', encoding='UTF-8')
+            return file
+        except FileExistsError:
+            stamp = calendar.timegm(datetime.utcnow().utctimetuple())
+            print(f'File {filename_save}.txt already exists. Stamp {stamp} added to filename')
+            filename_save = filename_save + '_' + str(stamp)
+            file = open(f'{filename_save}.txt', 'x', encoding='UTF-8')
+            return file
+
+
+txt_file = FileParser('PythonTest.txt')
+txt_file.split_lines_by_language()
